2f08fae111e4a8e568964e5dca913d607ce030bf
tugcecatakk <169039092+tugcecatakk@users.noreply.github.com>
2025-10-20 13:19:50 +0300

    Hakkımda sayfası yapıldı

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 bg-white bg-opacity-95 backdrop-blur-sm h-20 w-full flex items-center justify-between border-b border-gray-400 rounded-md shadow-sm z-50'>
+    <div className='fixed top-0 bg-[#faf8f5] bg-opacity-95 backdrop-blur-sm h-20 w-full flex items-center justify-between border-b border-gray-400 rounded-md shadow-sm z-50'>
       <div className='ml-3 text-3xl '>
         <h1 className='font-serif bg-gradient-to-r from-orange-500  to-pink-500 bg-clip-text text-transparent'>TeknoHub</h1>
       </div>
       <div className='font-sans text-md space-x-10 '>
         <Link to="/">Anasayfa</Link>
         <Link to="/categories">Kategoriler</Link>
-        <Link to="/">Hakkımda</Link>
-        <Link to="/">İletişim</Link>
+        <Link to="/about">Hakkımda</Link>
+        <Link to="/contact">İletişim</Link>
       </div>
       
       <div className='space-x-4'>
